82bdd9989f46aa95c29d5a7324bcbce7b05524be
p <josuelml28@hotmail.com>
2023-10-25 18:02:40 -0500

    Por siesque cambie algo

--- vite-project/src/Components/NavBar/index.jsx
+++ vite-project/src/Components/NavBar/index.jsx
@@ -5,51 +5,56 @@ import TelephoneIcon from "../../assets/telephone.svg";
 
 function NavBar() {
   return (
-    <nav className="">
+    <>
       <div className="h-7 flex justify-end items-center relative px-9 borde">
         <h4 className="font-tittle text-base w-fit absolute left-1/2 -translate-x-1/2">
           Siguenos en nuestras redes
         </h4>
-          <a href="#" className="h-5">
-            <img
-              src={FacebookIcon}
-              alt="Agua de Mesa San Andrés"
-              className="h-5"
-            />
-          </a>
-          <a href="#" className="h-5">
-            <img
-              src={InstagramIcon}
-              alt="Agua de Mesa San Andrés"
-              className="h-5"
-            />
-          </a>
-          <a href="#" className="h-5">
-            <img
-              src={TelephoneIcon}
-              alt="Agua de Mesa San Andrés"
-              className="h-5"
-            />
-          </a>
-          <a href="#" className="h-5">
-            <p className="italic underline text-links text-sm">946 508 111</p>
-          </a>
+        <a href="#" className="h-5">
+          <img
+            src={FacebookIcon}
+            alt="Agua de Mesa San Andrés"
+            className="h-5"
+          />
+        </a>
+        <a href="#" className="h-5">
+          <img
+            src={InstagramIcon}
+            alt="Agua de Mesa San Andrés"
+            className="h-5"
+          />
+        </a>
+        <a href="#" className="h-5">
+          <img
+            src={TelephoneIcon}
+            alt="Agua de Mesa San Andrés"
+            className="h-5"
+          />
+        </a>
+        <a href="#" className="h-5">
+          <p className="italic underline text-links text-sm">946 508 111</p>
+        </a>
       </div>
-      <div className="font-tittle h-24 relative flex justify-between items-center overflow-hidden bg-sanAndresBg px-7 text-white text-lg ">
+      <nav className=" font-tittle h-24 relative flex justify-between items-center overflow-hidden bg-sanAndresBg px-7 text-white text-lg ">
         <ul className="flex gap-8">
           <li>Nosotros</li>
           <li>¿Porque elegirnos?</li>
           <li>Proceso</li>
           <li>Nuestro Equipo</li>
         </ul>
-        <img src={logo} alt="Agua De Mesa San Andres" className="w-52 absolute left-1/2 -translate-x-1/2" />
+        <img
+          src={logo}
+          alt="Agua De Mesa San Andres"
+          className="w-52 absolute left-1/2 -translate-x-1/2"
+        />
         <ul className="flex gap-8">
           <li>Productos y precio</li>
           <li>Promociones</li>
           <li>Preguntas frecuentes</li>
         </ul>
-      </div>
-    </nav>
+      </nav>
+      {/* sticky top-0 z-[1] - > Agregar al nav y modificar su height*/}
+    </>
   );
 }
 
